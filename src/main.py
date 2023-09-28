ae1b33d7eceb05fac596f814629d28ce796649b1
sponkurtus2 <sponkurtus3@gmai.com>
2023-09-28 13:46:03 -0600

    feat: input_file open function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-# Data input
+import json
+
+
 name = input("What is your name: ")
 last_name = input("And your last name: ")
 
@@ -10,5 +12,14 @@ user = {
 
 file_name = input("File name: ")
 
-with open(f"{file_name}.json", "w+") as file:
-    file.write(user)
+# Creates a json with the input data
+
+with open(f"{file_name}.json", "w+") as output_file:
+    data = json.dumps(user, indent=4)
+    output_file.write(data)
+
+# Loads the json file and then print it on the console
+
+with open(f"{file_name}.json", "r") as input_file:
+    data = json.load(input_file)
+    print(data)
